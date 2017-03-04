f7603333a65638c9ee387b2e0ef8223c54d667ab
Mafruroh <urochvhs4mlg@gmail.com>
2017-03-03 21:36:47 -0800

    codeigniter
    PraktikumFramework

---
+++ codeigniter/application/views/about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MafrurohBlog</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">GaYoungBlog</a>
+            </div>
+        
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="<?php echo site_url()?>/home"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                <li><a href="<?php echo site_url()?>/about"><span class="glyphicon glyphicon-user"></span>About</a></li>
+                <li><a href="<?php echo site_url()?>/contact"><span class="glyphicon glyphicon-phone-alt"></span>Contact</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Administrator <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div>
+        </nav>
+          <div class="text-center"><h1>About</h1></div>
+        <div class="jumbotron">
+          <div class="container-fluid text-center">
+            <ul class="list-group">
+            <li class="list-group-item list-group-item-success">MAFRUROH</li>
+            <li class="list-group-item list-group-item-info">1541180230</li>
+            <li class="list-group-item list-group-item-warning">D4 Teknik Informatika / 2B</li>
+            <li class="list-group-item list-group-item-danger">Politeknik Negeri Malang</li>
+          </ul>
+          <div class="list-group">
+            <li class="list-group-item list-group-item-success">Jl. Sartono SH2 Kota Malang, Jawa Timur</li>
+            <li class="list-group-item list-group-item-info">087849770406</li>
+            <li class="list-group-item list-group-item-warning">Menonton Drama Korea Sebagai Hobby</li>
+            <li class="list-group-item list-group-item-danger">Park Ga Young</li>
+          <div class="list-group">
+            <li class="list-group-item list-group-item-danger">Informasi lebih lanjut, silahkan klik dibawah ini<span class="glyphicon glyphicon-hand-down"></span></li>
+-
+          <ul class="list-inline intro-social-buttons text-buttom">
+                            <li>
+                                <a href="https://twitter.com/" class="label label-info">Twitter</a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/" class="label label-primary">Facebook</a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/Mafruroh" class="label label-default">Github</a>
+                            </li>
+                        </ul>
+        </div>
+      </div>
+    </div>
+</div>
+    <!-- jQuery -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+     <script src="Hello World"></script>
+  </body>
+</html>
---
+++ codeigniter/application/views/home.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MafrurohBlog</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">GaYoungBlog</a>
+            </div>
+        
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="<?php echo site_url()?>/home"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                <li><a href="<?php echo site_url()?>/about"><span class="glyphicon glyphicon-user"></span>About</a></li>
+                <li><a href="<?php echo site_url()?>/contact"><span class="glyphicon glyphicon-phone-alt"></span>Contact</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Administrator <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div>
+        </nav>
+        <div class="jumbotron">
+          <div class="container">
+          <div class="row">
+</div>
+            <h1>Annyeong, Selamat Datang!</h1>
+            <p>Terimakasih telah berkunjung dan menyempatkan untuk melihat GaYoungBlog, untuk info lebih lanjut silahkan klik button About dan Contact. ddo mannayo chingudeul ...</p>
+            <p>
+              <a class="btn btn-primary btn-lg">Learn more »</a>
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          Annyeong !! Bagaimana kabar kalian? semoga baik-baik saja ya, perkenalkan nama saya Mafruroh saya berasal dari Malang, kerjaan saya sehari-hari makan kuliah nugas rapat latihan sholat tidur dan berlanjut setiap harinya. Kebiasaan saya kalau free biasanya nonton drama korea karna saya juga penggemar drama korea, hampir semua drama korea saya tontonin sampe stock drama korea abis saya gak bakalan berhenti buat nonton yang namanya drama korea.
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          Jangan tertarik kepada seseorang kerna parasnya, sebab keelokan paras dapat menyesatkan. Jangan pula tertarik kepada kekayaannya kerna kekayaan dapat musnah. Tertariklah kepada seseorang yang dapat membuatmu tersenyum, kerna hanya senyum yang dapat membuat hari-hari yang gelap menjadi cerah.
+        </div>
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+     <script src="Hello World"></script>
+  </body>
+</html>
